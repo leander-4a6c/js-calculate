0b08cd9af089a5de0638ac8fb481e2f9c96fa18e
leanderVanDerOest <leander1999@me.com>
2017-06-14 09:45:00 +0200

    style gemaakt
    Signed-off-by: leanderVanDerOest <leander1999@me.com>

---
+++ script.js
@@ -0,0 +1,28 @@
+function optellen(){
+	var array;
+	var words = document.getElementById('getallen').value;
+	array = (words.split(","));
+	var operator = document.getElementById('keuze').value;
+
+ 
+	for(i=0; i <array.length; i++){
+		 array[i] = parseFloat(array[i]);
+
+	}
+
+	var result = array[0];
+	for(i=1; i<array.length; i++){
+		result = berekenen(result,array[i],operator);
+
+	}
+
+	function berekenen(nummer,nummer2,operator){
+		if (operator == '+') {
+            result = nummer + nummer2;
+
+        } 
+        return result;
+    }
+    console.log(result);
+    document.getElementById('result').innerHTML = "result:" + result;
+}
